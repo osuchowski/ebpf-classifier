0daa0e229e2da0416fe3b4504e15b8985f36dca6
Dawid Osuchowski <dawid@osuchowski.space>
2026-09-20 23:31:26 +0200

    port Welford algorithm to filter_xdp.py and dt_filter_xdp.py
    Signed-off-by: Dawid Osuchowski <dawid@osuchowski.space>

--- filter/filter_xdp.py
+++ filter/filter_xdp.py
@@ -26,6 +26,7 @@ bpf_text = """
 #define DEBUG_BUILD
 #define NUM_FEATURES 12
 #define FIXED_POINT_DIGITS 16
+#define FXP_VALUE FIXED_POINT_DIGITS
 
 struct pkt_key_t {
   u32 protocol;
@@ -35,16 +36,74 @@ struct pkt_key_t {
   u32 dport;
 };
 
+struct welford_stat_t {
+  u64 sum;
+  u64 m2;
+};
+
 struct pkt_leaf_t {
   u32 num_packets;
   u64 last_packet_timestamp;
-  u32 saddr;
-  u32 daddr;
   u32 sport;
   u32 dport;
-  u64 features[6];
-  bool is_anomaly;
+  struct welford_stat_t stats[3];
 };
+
+enum flow_feature_idx {
+  FEAT_TOT_LEN   = 0,
+  FEAT_INTERVAL  = 1,
+  FEAT_DIRECTION = 2,
+  NUM_FLOW_FEATS = 3
+};
+
+#define IDX_RAW_OFFSET   3
+#define IDX_MEAN_OFFSET  6
+#define IDX_SQCV_OFFSET  9
+
+static __always_inline int64_t fxp_mul(int64_t a, int64_t b) {
+  if ((a > -(1LL << 30)) && (a < (1LL << 30)) &&
+      (b > -(1LL << 30)) && (b < (1LL << 30))) {
+    return (a * b) >> FXP_VALUE;
+  }
+  return (a >> (FXP_VALUE / 2)) * (b >> (FXP_VALUE / 2));
+}
+
+static __always_inline int64_t fxp_div_sqcv(uint64_t variance, uint64_t mean_sq) {
+  if (mean_sq == 0) return 0;
+  if (variance < (1ULL << 46)) {
+    return (int64_t)((variance << FXP_VALUE) / mean_sq);
+  }
+  uint64_t denom = mean_sq >> FXP_VALUE;
+  return (denom > 0) ? (int64_t)(variance / denom) : 0;
+}
+
+static __always_inline void update_welford_feature(struct welford_stat_t *stat,
+                                                   uint64_t num_packets,
+                                                   int64_t new_val,
+                                                   int64_t *out_mean,
+                                                   int64_t *out_sqcv) {
+  uint64_t n_prev = num_packets - 1;
+  int64_t old_mean = (int64_t)(stat->sum / n_prev);
+
+  stat->sum += (uint64_t)new_val;
+  int64_t new_mean = (int64_t)(stat->sum / num_packets);
+  *out_mean = new_mean;
+
+  int64_t delta1 = new_val - old_mean;
+  int64_t delta2 = new_val - new_mean;
+  int64_t term = fxp_mul(delta1, delta2);
+  if (term > 0) {
+    stat->m2 += (uint64_t)term;
+  }
+
+  if (new_mean > 0) {
+    uint64_t variance = stat->m2 / n_prev;
+    uint64_t mean_sq = (uint64_t)fxp_mul(new_mean, new_mean);
+    *out_sqcv = fxp_div_sqcv(variance, mean_sq);
+  } else {
+    *out_sqcv = 0;
+  }
+}
 
 BPF_TABLE("lru_hash", struct pkt_key_t, struct pkt_leaf_t, sessions, 1024);
 BPF_HASH(dropcnt, int, u32);
@@ -60,7 +119,6 @@ int xdp_drop_packet(struct xdp_md *ctx) {
   struct tcphdr *th;
   struct udphdr *uh;
   struct pkt_key_t pkt_key = {};
-  struct pkt_leaf_t pkt_val = {};
 
 
   pkt_key.protocol = 0;
@@ -117,50 +175,51 @@ int xdp_drop_packet(struct xdp_md *ctx) {
       struct pkt_leaf_t zero = {};
       zero.sport = pkt_key.sport;
       zero.dport = pkt_key.dport;
-      zero.saddr = pkt_key.saddr;
-      zero.daddr = pkt_key.daddr;
       zero.num_packets = 0;
       zero.last_packet_timestamp = ts;
       sessions.update(&pkt_key, &zero);
       pkt_leaf = sessions.lookup(&pkt_key);
     }
     if (pkt_leaf != NULL) {
+      int64_t feat[NUM_FEATURES] = {0};
       pkt_leaf->num_packets += 1;
-      int64_t sport = pkt_leaf->sport;
-      int64_t dport = pkt_leaf->dport;
-      int64_t protocol = iph->protocol;
-      int64_t tot_len = ntohs(iph->tot_len);
-      int64_t interval_time = 0;
+      feat[0] = pkt_leaf->sport;
+      feat[1] = pkt_leaf->dport;
+      feat[2] = iph->protocol;
+      feat[3] = ntohs(iph->tot_len);
+      feat[4] = 0;
       if (pkt_leaf->last_packet_timestamp > 0) {
-        interval_time = ts - pkt_leaf->last_packet_timestamp;
+        feat[4] = ts - pkt_leaf->last_packet_timestamp;
       }
       pkt_leaf->last_packet_timestamp = ts;
-      int64_t direction = pkt_key.sport == sport;
+      feat[5] = (pkt_key.sport == feat[0]);
 
-      sport <<= FIXED_POINT_DIGITS;
-      dport <<= FIXED_POINT_DIGITS;
-      protocol <<= FIXED_POINT_DIGITS;
-      tot_len <<= FIXED_POINT_DIGITS;
-      interval_time <<= FIXED_POINT_DIGITS;
-      direction <<= FIXED_POINT_DIGITS;
+      feat[0] <<= FXP_VALUE;
+      feat[1] <<= FXP_VALUE;
+      feat[2] <<= FXP_VALUE;
+      feat[3] <<= FXP_VALUE;
+      feat[4] <<= FXP_VALUE;
+      feat[5] <<= FXP_VALUE;
 
-      pkt_leaf->features[0] += tot_len;
-      pkt_leaf->features[1] += interval_time;
-      pkt_leaf->features[2] += direction;
+      if (pkt_leaf->num_packets > 1) {
+        #pragma unroll
+        for (int i = 0; i < NUM_FLOW_FEATS; i++) {
+          update_welford_feature(&pkt_leaf->stats[i],
+                                 pkt_leaf->num_packets,
+                                 feat[IDX_RAW_OFFSET + i],
+                                 &feat[IDX_MEAN_OFFSET + i],
+                                 &feat[IDX_SQCV_OFFSET + i]);
+        }
+      } else {
+        #pragma unroll
+        for (int i = 0; i < NUM_FLOW_FEATS; i++) {
+          pkt_leaf->stats[i].sum = (uint64_t)feat[IDX_RAW_OFFSET + i];
+          pkt_leaf->stats[i].m2 = 0;
+          feat[IDX_MEAN_OFFSET + i] = feat[IDX_RAW_OFFSET + i];
+          feat[IDX_SQCV_OFFSET + i] = 0;
+        }
+      }
 
-      int64_t avg_tot_len       = pkt_leaf->features[0]/pkt_leaf->num_packets;
-      int64_t avg_interval_time = pkt_leaf->features[1]/pkt_leaf->num_packets;
-      int64_t avg_direction     = pkt_leaf->features[2]/pkt_leaf->num_packets;
-
-      pkt_leaf->features[3] += abs(tot_len - avg_tot_len);
-      pkt_leaf->features[4] += abs(interval_time - avg_interval_time);
-      pkt_leaf->features[5] += abs(direction - avg_direction);
-
-      int64_t avg_dev_tot_len       = pkt_leaf->features[3]/pkt_leaf->num_packets;
-      int64_t avg_dev_interval_time = pkt_leaf->features[4]/pkt_leaf->num_packets;
-      int64_t avg_dev_direction     = pkt_leaf->features[5]/pkt_leaf->num_packets;
-
-      int64_t feat[NUM_FEATURES] = {sport, dport, protocol, tot_len, interval_time, direction, avg_tot_len, avg_interval_time, avg_direction, avg_dev_tot_len, avg_dev_interval_time, avg_dev_direction};
       sessions.update(&pkt_key, pkt_leaf);
       int _zero = 0;
       u32 val = 0, *vp;
